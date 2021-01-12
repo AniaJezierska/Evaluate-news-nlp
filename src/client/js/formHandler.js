9ab48f61c37548ed111c55662270534a070973a0
AniaJezierska <candyrockstore5@gmail.com>
2021-01-12 15:21:08 +0100

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,31 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    const formText = document.getElementById('name').value;
+    console.log(formText);
+    if (Client.checkUrl(formText) == true) {
+        fetch('http://localhost:8081/analyse', {
+                method: 'POST',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    url: formText
+                })
+            })
+            .then(response => response.json())
+            .then(function (response) {
+                document.getElementById('polarity').innerHTML = response.polarity
+                document.getElementById('subjectivity').innerHTML = response.subjectivity
+                document.getElementById('polarity-confidence').innerHTML = response.polarity_confidence
+                document.getElementById('subjectivity-confidence').innerHTML = response.subjectivity_confidence
+            })
+    } else {
+        console.log('Invalid URL');
+    }
 }
 
-export { handleSubmit }
+export {
+    handleSubmit
+}
