17ecd140249381f562e0b2f3b52768617c032ada
AniaJezierska <candyrockstore5@gmail.com>
2021-01-17 12:12:42 +0100

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,64 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(express.static('dist'))
+var aylien = require('aylien_textapi');
 
-console.log(__dirname)
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+//start instance of app
+const app = express();
+
+//start directory
+app.use(express.static('dist'));
+
+app.use(bodyParser.json());
+
+// cors for cross origin allowance
+app.use(cors());
+
+//set aylien API credentials
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+
+console.log(`Your API KEY is ${process.env.API_KEY}`);
+
+let aylienRes = {};
+
+console.log(__dirname);
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('App listening on port 8081!');
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.send(mockAPIResponse);
 })
+
+app.post('/analyse', function (req, res) {
+    textapi.sentiment({
+        url: req.body.url
+    }, (error, response) => {
+        if (error == null) {
+            aylienRes = {
+                'polarity': response.polarity,
+                'subjectivity': response.subjectivity,
+                'text': response.text,
+                'polarity_confidence': response.polarity_confidence,
+                'subjectivity_confidence': response.subjectivity_confidence
+            }
+            res.send(aylienRes);
+            console.log(aylienRes);
+        } else {
+            console.log('error', error);
+            alert('error');
+        }
+    })
+});
